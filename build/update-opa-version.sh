56d74ff7aad934c59154658c2ebf7722c7c8aa99
Ashutosh Narkar <anarkar4387@gmail.com>
2018-04-25 12:06:08 -0700

    Add missing comma after image version
    Signed-off-by: Ashutosh Narkar <anarkar4387@gmail.com>

--- build/update-opa-version.sh
+++ build/update-opa-version.sh
@@ -29,7 +29,7 @@ if [ $? -eq 0 ]; then
   sed -i '' "s/openpolicyagent\/opa:.*/openpolicyagent\/opa:$tag-istio/" README.md
 
   # update plugin image version in quick_start.yaml
-  sed -i '' "/opa_container/{N;s/openpolicyagent\/opa:.*/openpolicyagent\/opa:$tag-istio\"/;}" quick_start.yaml
+  sed -i '' "/opa_container/{N;s/openpolicyagent\/opa:.*/openpolicyagent\/opa:$tag-istio\"\,/;}" quick_start.yaml
 
   # run glide update
   glide up -v
