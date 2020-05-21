57ba0973cd51cbd096aa1136ed3a09e6afdedba4
opa-updater-automation <opa-updater-automation@openpolicyagent.org>
2020-05-21 13:35:41 +0000

    Update OPA version to v0.20.1
    Signed-off-by: opa-updater-automation <opa-updater-automation@openpolicyagent.org>

--- vendor/github.com/open-policy-agent/opa/cmd/test.go
+++ vendor/github.com/open-policy-agent/opa/cmd/test.go
@@ -70,7 +70,7 @@ If the '--bundle' option is specified the paths will be treated as policy bundle
 and loaded following standard bundle conventions. The path can be a compressed archive
 file or a directory which will be treated as a bundle. Without the '--bundle' flag OPA
 will recursively load ALL *.rego, *.json, and *.yaml files for evaluating the test cases.
-	
+
 Example policy (example/authz.rego):
 
 	package authz
@@ -329,13 +329,14 @@ func filterTrace(params *testCommandParams, trace []*topdown.Event) []*topdown.E
 func init() {
 	testCommand.Flags().BoolVarP(&testParams.verbose, "verbose", "v", false, "set verbose reporting mode")
 	testCommand.Flags().BoolVarP(&testParams.failureLine, "show-failure-line", "l", false, "show test failure line")
+	testCommand.Flags().MarkDeprecated("show-failure-line", "use -v instead")
 	testCommand.Flags().DurationVarP(&testParams.timeout, "timeout", "t", time.Second*5, "set test timeout")
 	testCommand.Flags().VarP(testParams.outputFormat, "format", "f", "set output format")
 	testCommand.Flags().BoolVarP(&testParams.coverage, "coverage", "c", false, "report coverage (overrides debug tracing)")
 	testCommand.Flags().Float64VarP(&testParams.threshold, "threshold", "", 0, "set coverage threshold and exit with non-zero status if coverage is less than threshold %")
-	testCommand.Flags().BoolVarP(&testParams.bundleMode, "bundle", "b", false, "load paths as bundle files or root directories")
 	testCommand.Flags().BoolVar(&testParams.benchmark, "bench", false, "benchmark the unit tests")
 	testCommand.Flags().StringVarP(&testParams.runRegex, "run", "r", "", "run only test cases matching the regular expression.")
+	addBundleModeFlag(testCommand.Flags(), &testParams.bundleMode, false)
 	addBenchmemFlag(testCommand.Flags(), &testParams.benchMem, true)
 	addCountFlag(testCommand.Flags(), &testParams.count, "test")
 	addMaxErrorsFlag(testCommand.Flags(), &testParams.errLimit)
