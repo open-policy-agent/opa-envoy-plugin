ebbbab93f8601d140d2902a49c61ecbba73dacd1
Stephan Renatus <srenatus@chef.io>
2020-07-07 15:43:21 +0200

    plugin: move into package
    When the definitions moved here live in a `main` package, they cannot be
    imported by anyone else. Since the stuff they _use_ lives in an `internal`
    package, you cannot defined them in your own code.
    
    Hence, there's no way to combine this plugin with another one, since you
    cannot create the new cmd's code using the code here.
    
    With this change, the same thing that happens in cmd/opa-istio-plugin/main.go
    could happen in another main.go that also registers another plugin.
    
    Signed-off-by: Stephan Renatus <srenatus@chef.io>

--- cmd/opa-istio-plugin/main.go
+++ cmd/opa-istio-plugin/main.go
@@ -8,29 +8,14 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/open-policy-agent/opa-istio-plugin/internal"
+	"github.com/open-policy-agent/opa-istio-plugin/plugin"
 	"github.com/open-policy-agent/opa/cmd"
-	"github.com/open-policy-agent/opa/plugins"
 	"github.com/open-policy-agent/opa/runtime"
 )
 
-// Factory defines the interface OPA uses to instantiate a plugin.
-type Factory struct{}
-
-// New returns the object initialized with a valid plugin configuration.
-func (Factory) New(m *plugins.Manager, config interface{}) plugins.Plugin {
-	return internal.New(m, config.(*internal.Config))
-}
-
-// Validate returns a valid configuration to instantiate the plugin.
-func (Factory) Validate(m *plugins.Manager, config []byte) (interface{}, error) {
-	return internal.Validate(m, config)
-}
-
 func main() {
-
-	runtime.RegisterPlugin("envoy.ext_authz.grpc", Factory{}) // for backwards compatibility
-	runtime.RegisterPlugin(internal.PluginName, Factory{})
+	runtime.RegisterPlugin("envoy.ext_authz.grpc", plugin.Factory{}) // for backwards compatibility
+	runtime.RegisterPlugin(plugin.PluginName, plugin.Factory{})
 
 	if err := cmd.RootCommand.Execute(); err != nil {
 		fmt.Println(err)
---
+++ plugin/plugin.go
@@ -0,0 +1,27 @@
+// Copyright 2020 The OPA Authors. All rights reserved.
+// Use of this source code is governed by an Apache2
+// license that can be found in the LICENSE file.
+
+package plugin
+
+import (
+	"github.com/open-policy-agent/opa/plugins"
+
+	"github.com/open-policy-agent/opa-istio-plugin/internal"
+)
+
+// Factory defines the interface OPA uses to instantiate a plugin.
+type Factory struct{}
+
+// PluginName is the name to register with the OPA plugin manager
+const PluginName = internal.PluginName
+
+// New returns the object initialized with a valid plugin configuration.
+func (Factory) New(m *plugins.Manager, config interface{}) plugins.Plugin {
+	return internal.New(m, config.(*internal.Config))
+}
+
+// Validate returns a valid configuration to instantiate the plugin.
+func (Factory) Validate(m *plugins.Manager, config []byte) (interface{}, error) {
+	return internal.Validate(m, config)
+}
