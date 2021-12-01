337ae50fec58d08d4e3687081da4e32873df287f
Stephan Renatus <stephan.renatus@gmail.com>
2021-12-01 12:17:24 +0100

    build: use vendored opa dir to determine plugin ref
    Signed-off-by: Stephan Renatus <stephan.renatus@gmail.com>

--- build/get-plugin-rev.sh
+++ build/get-plugin-rev.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Script to get number of commits from the last OPA revendoring
 
-GIT_SHA=$(git log -n 1 --oneline  --pretty=format:"%h" --author=dependabot)
+GIT_SHA=$(git log -n 1 --pretty=format:%H -- vendor/github.com/open-policy-agent/opa)
 COMMITS=$(git rev-list $GIT_SHA..HEAD --count)
 
 if [ $COMMITS -ne 0 ]; then
