a9566998a17d1c5cb308b133eab0e163d1fe2fb3
Andrea Falconi <andrea.falconi@gmail.com>
2022-04-24 12:40:32 +0200

    add go tools to dev env.

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,8 +1,8 @@
 {
   inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
     nixie.url = "github:c0c0n3/nixie";
     gomod2nix.url = "github:tweag/gomod2nix";
-    nixpkgs.follows = "nixie/nixpkgs";
   };
 
   outputs = { self, nixpkgs, nixie, gomod2nix }:
@@ -12,8 +12,13 @@
       defaultPackage.${system} = with sysPkgs; buildEnv {
         name = "opa-dev-env";
         paths = [
-            go
-            gomod2nix.defaultPackage.${system}
+          # Go dev env:
+          # - Compiler
+          # - VS Code tools (https://github.com/golang/vscode-go#tools)
+          # - Nix build tool (https://www.tweag.io/blog/2021-03-04-gomod2nix/)
+          go gopls delve go-tools go-outline
+          # NOTE. delve includes dlv-dap; go-tools = staticcheck
+          gomod2nix.defaultPackage.${system}
         ];
       };
     };
